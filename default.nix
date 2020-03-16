6c469ea20aff7ff648bfcb8c3294ad3df6c830a3
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-03-16 23:27:20 +0100

    change default.nix

--- default.nix
+++ default.nix
@@ -1,18 +1,24 @@
-#{stdenv, gcc, coreutils}:
-with (import <nixpkgs>{});
-
-stdenv.mkDerivation {
-  pname = "hgen";
-  version = "1.0";
-  src = ./.;
-  buildInputs = [
-    gcc
-    coreutils
-  ];
-  installPhase =''
-    mkdir $out
-    mkdir $out/bin
-    make
-    cp hgen $out/bin
-  '';
+# import <nixpkgs>{} {
+#{stdenv, gcc, coreutils}: {
+# with (import <nixpkgs>{}): {
+# import <nixpkgs>{} : {
+let
+    pkgs = import <nixpkgs> {};
+    stdenv = pkgs.stdenv;
+in with pkgs; {
+  hgen = stdenv.mkDerivation {
+    pname = "hgen";
+    version = "1.0";
+    src = ./.;
+    buildInputs = [
+      gcc
+      coreutils
+    ];
+    installPhase =''
+      mkdir $out
+      mkdir $out/bin
+      make
+      cp hgen $out/bin
+    '';
+  };
 }
